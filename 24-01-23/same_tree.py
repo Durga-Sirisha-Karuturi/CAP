1b82589aceec5a421f3a53f5a12d61994a20e25b
Durga-Sirisha-Karuturi <84554281+Durga-Sirisha-Karuturi@users.noreply.github.com>
2023-01-24 16:20:44 +0530

    Update same_tree.py

--- 24-01-23/same_tree.py
+++ 24-01-23/same_tree.py
@@ -1,17 +1,8 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        flag=False
         if not(p) and not(q):
             return True
         if not(p) or not(q) or p.val!=q.val:
             return False
-        if self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right):
-            flag=True
-        return flag
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
         
