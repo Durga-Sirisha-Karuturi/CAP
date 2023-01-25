a62d75e86e1f4aa9d9c5d29f622b1d0c7ef484a3
Durga-Sirisha-Karuturi <84554281+Durga-Sirisha-Karuturi@users.noreply.github.com>
2023-01-25 17:03:53 +0530

    Create lca.py

---
+++ 25-01-23/lca.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root==None:
+            return None
+        if root.val==p.val or root.val==q.val:
+            return root
+        left=self.lowestCommonAncestor(root.left,p,q)
+        right=self.lowestCommonAncestor(root.right,p,q)
+        if left!=None and right!=None:
+            return root
+        elif left!=None:
+            return left
+        else:
+            return right
