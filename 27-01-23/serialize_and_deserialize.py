272baff014711f9cb27ed906c4571319180d3be8
Durga-Sirisha-Karuturi <84554281+Durga-Sirisha-Karuturi@users.noreply.github.com>
2023-01-28 04:17:51 +0530

    Update serialize_and_deserialize.py

--- 27-01-23/serialize_and_deserialize.py
+++ 27-01-23/serialize_and_deserialize.py
@@ -6,22 +6,23 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        res=[]
         if not root:
-            res.append("NO")
-        l=[root] 
-        res.append(str(root.val))
+            return "N"
+        res=[]
+        l=[root]
+        res=[str(root.val)]
         while(l):
-            for i in range(0,len(l)):
+            length=len(l)
+            for i in range(0,length):
                 cur=l[0]
                 l.remove(cur)
-                if cur.left:
-                    l.append(cur.left)
-                    res.append(str(cur.left.val))
-                if cur.right:
-                    l.append(cur.right)
-                    res.append(str(cur.right.val))
+                if not cur:
+                    res.append("N")
+                res.append(str(cur.val))
+                l.append(cur.left)
+                l.append(cur.right)
         return ",".join(res)
+        
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -29,11 +30,11 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        value=iter(data.split(","))
+        val=iter(data.split(","))
         def des():
-            if value=="NO":
+            if val=="N":
                 return None
-            node= TreeNode(int(value))
+            node=TreeNode(str(val))
             node.left=des()
             node.right=des()
             return node
