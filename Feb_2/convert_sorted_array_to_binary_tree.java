583cd7a5b00b934ee12d3e696b90e4716f401f26
Durga-Sirisha-Karuturi <84554281+Durga-Sirisha-Karuturi@users.noreply.github.com>
2023-02-02 21:15:10 +0530

    Create convert_sorted_array_to_binary_tree.java

---
+++ Feb_2/convert_sorted_array_to_binary_tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    TreeNode bst(int[] nums,int start,int end){
+        if(start>end){
+            return null;
+        }
+        int mid= (start+end)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left=bst(nums,start,mid-1);
+        node.right=bst(nums,mid+1,end);
+        return node;
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return bst(nums,0,nums.length-1);
+    }
